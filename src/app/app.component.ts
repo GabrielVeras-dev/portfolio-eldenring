6796299c64fd5135b7e44b6c535816b460faeadf
Gabriel Veras <bielveras.e@hotmail.com>
2026-04-08 17:22:28 -0300

    feat: adicionado seções sobre e projetos, criado pasta assets e inserido imagens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,18 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from './core/navbar/navbar.component';
 import { HeroComponent } from './sections/hero/hero.component';
 import { SobreComponent } from './sections/sobre/sobre.component';
+import { ProjetosComponent } from './sections/projetos/projetos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, HeroComponent, SobreComponent],
+  imports: [NavbarComponent, HeroComponent, SobreComponent, ProjetosComponent],
   template: `
     <app-navbar />
     <main>
       <app-hero />
       <app-sobre />
+      <app-projetos />
     </main>
   `
 })
---
+++ src/app/sections/projetos/projetos.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Project {
+  title: string;
+  description: string;
+  techs: string[];
+  githubUrl: string;
+  demoUrl?: string;
+  imgUrl: string;
+}
+
+@Component({
+  selector: 'app-projetos',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './projetos.component.html',
+  styleUrl: './projetos.component.scss'
+})
+export class ProjetosComponent {
+  projects: Project[] = [
+    {
+      title: 'Rick and Morty API',
+      description: 'API REST em Java consumindo a API pública do Rick and Morty...',
+      techs: ['Java 17', 'Spring Boot', 'REST API', 'Maven'],
+      githubUrl: 'https://github.com/GabrielVeras-dev/rickandmorty-frontend',
+      imgUrl: 'assets/images/rickandmorty.jpeg'
+    },
+    {
+      title: 'Finanças API',
+      description: 'API backend para gerenciamento financeiro com CRUD de entidades, regras de negócio, arquitetura REST e estrutura preparada para integração com frontend.',
+      techs: ['Java', 'Spring Boot', 'JPA/Hibernate', 'REST API'],
+      githubUrl: 'https://github.com/GabrielVeras-dev',
+      imgUrl: 'assets/images/financasapi.jpeg'
+    },
+    {
+      title: 'Java OOP Practice',
+      description: 'Projeto dedicado à prática dos pilares da POO: Encapsulamento, Herança, Polimorfismo e Abstração, com modelagem orientada a objetos.',
+      techs: ['Java 17', 'POO', 'Design de Classes'],
+      githubUrl: 'https://github.com/GabrielVeras-dev',
+      imgUrl: 'assets/images/javapoo.jpeg'
+    },
+    {
+      title: 'Java Core Practice',
+      description: 'Projeto focado nos fundamentos do Core Java: estruturas de controle, Collections Framework, manipulação de objetos e tratamento de exceções.',
+      techs: ['Java 17', 'Collections', 'POO'],
+      githubUrl: 'https://github.com/GabrielVeras-dev',
+      imgUrl: 'assets/images/javacore.jpeg'
+    }
+  ];
+}
